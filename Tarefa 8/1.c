842f4c873a304df2dc16b4e1abcab48b1b7fcfac
nataliasantosfarias <nataliafariassantos006@gmail.com>
2024-05-28 19:54:50 -0300

    Altera a fila para Pilha

--- Tarefa 8/1.c
+++ Tarefa 8/1.c
@@ -14,73 +14,60 @@ typedef struct tipo_carro {
 } Carro;
 
 typedef struct {
-    Carro *inicio;
-    Carro *fim;
-} Fila;
+    Carro *topo;
+} Pilha;
 
-void iniciar(Fila *fila) {
-    fila->inicio = NULL;
-    fila->fim = NULL;
+void iniciar(Pilha *pilha) {
+    pilha->topo = NULL;
 }
 
-//função adicionar
-void adicionar_carro(char nome[100], char cpf[12], char marca[50], char modelo[50], char placa[8], Fila *fila) {
+void adicionar_carro(char nome[100], char cpf[12], char marca[50], char modelo[50], char placa[8], Pilha *pilha) {
     Carro *novo = (Carro *)malloc(sizeof(Carro));
+    if (novo == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return;
+    }
     strcpy(novo->nome, nome);
     strcpy(novo->cpf, cpf);
     strcpy(novo->marca, marca);
     strcpy(novo->modelo, modelo);
     strcpy(novo->placa, placa);
-    novo->proximo = NULL;
+    novo->proximo = pilha->topo;
+    pilha->topo = novo;
 
-    if (fila->fim == NULL) {
-        fila->inicio = novo;
-        fila->fim = novo;
-    } else {
-        fila->fim->proximo = novo;
-        fila->fim = novo;
-    }
-
-    printf("%s - O seu carro adicionado na fila!\n", nome);
+    printf("%s - O seu carro foi adicionado na pilha!\n", nome);
 }
 
-//função remover
-void remover_carro(Fila *fila) {
-    if (fila->inicio == NULL) {
-        printf("A fila está vazia!\n");
+void remover_carro(Pilha *pilha) {
+    if (pilha->topo == NULL) {
+        printf("A pilha está vazia!\n");
         return;
     }
 
-    Carro *remover = fila->inicio;
-    fila->inicio = fila->inicio->proximo;
-
-    if (fila->inicio == NULL) {
-        fila->fim = NULL;
-    }
+    Carro *remover = pilha->topo;
+    pilha->topo = pilha->topo->proximo;
 
     printf("Carro removido: %s, Placa: %s\n", remover->nome, remover->placa);
     free(remover);
-    // a função free é usada para liberar a memória
 }
 
-// usada para listar os carros
-void listar_carros(Fila *fila) {
-    if (fila->inicio == NULL) {
-        printf("A fila está vazia!\n");
+void listar_carros(Pilha *pilha) {
+    if (pilha->topo == NULL) {
+        printf("A pilha está vazia!\n");
         return;
     }
 
-    Carro *atual = fila->inicio;
-    printf("\n\n========= FILA =========\n");
+    Carro *atual = pilha->topo;
+    printf("\n\n========= PILHA =========\n");
     while (atual != NULL) {
         printf("Nome: %s, CPF: %s, Marca: %s, Modelo: %s, Placa: %s\n", atual->nome, atual->cpf, atual->marca, atual->modelo, atual->placa);
         atual = atual->proximo;
     }
-    printf("========= FILA =========\n");
+    printf("========= PILHA =========\n");
 }
 
 int main() {
-    Fila vaga1, vaga2;
+    Pilha vaga1, vaga2;
     iniciar(&vaga1);
     iniciar(&vaga2);
 
@@ -96,15 +83,15 @@ int main() {
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
 
-        if (opcao == 0) { // Se opção for 0 PARE;
-            printf("Você escolheu sair!, \n obrigada volte sempre!");
+        if (opcao == 0) {
+            printf("Você escolheu sair! Obrigado, volte sempre!\n");
             break;
         }
 
-        if (opcao >= 1 && opcao <= 3) { // Se condição maior ou igual 1, e menor ou igual à 3...
+        if (opcao >= 1 && opcao <= 3) {
             printf("Escolha a vaga (1 ou 2): ");
             scanf("%d", &vaga);
-            Fila *fila_selecionada = (vaga == 1) ? &vaga1 : &vaga2;
+            Pilha *pilha_selecionada = (vaga == 1) ? &vaga1 : &vaga2;
 
             switch (opcao) {
                 case 1:
@@ -118,13 +105,13 @@ int main() {
                     scanf(" %[^\n]", modelo);
                     printf("Digite a placa do carro: ");
                     scanf(" %s", placa);
-                    adicionar_carro(nome, cpf, marca, modelo, placa, fila_selecionada);
+                    adicionar_carro(nome, cpf, marca, modelo, placa, pilha_selecionada);
                     break;
                 case 2:
-                    remover_carro(fila_selecionada);
+                    remover_carro(pilha_selecionada);
                     break;
                 case 3:
-                    listar_carros(fila_selecionada);
+                    listar_carros(pilha_selecionada);
                     break;
                 default:
                     printf("Opcao invalida!\n");
